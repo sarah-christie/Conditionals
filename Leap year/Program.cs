0abc8dfabc930f7866a932b5cc5645b8132e37da
SazzerC <76841020+sarah-christie@users.noreply.github.com>
2021-03-11 18:01:47 +0000

    Add files via upload

---
+++ Leap year/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Leap_year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int year;
+            year = Convert.ToInt16(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 400 == 0) || (year % 4 == 0 && !(year % 100 == 0)))
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+                Console.WriteLine(year + " is NOT a leap year");
+            
+        }
+    }
+}
